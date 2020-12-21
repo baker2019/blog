bbbdd5d356dd70e335bacecf8c65d9f13be9d216
jason baker <bakerpools416@gmail.com>
2020-12-21 10:19:54 -0600

    added admin-interface

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_interface'
+    'admin_interface',
     'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
