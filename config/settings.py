a4be2929b756c3fdd784525a4c2a4a48dfd678f2
jason baker <bakerpools416@gmail.com>
2020-12-21 10:48:03 -0600

    added admin-interface

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*1gs=ta!i5)b1_udcb^n@ulo%t7nha7vpys_pb#4iufrid_b_z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://lake-conroy.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
